aa87bd231d26c3b52960520c349178c349c71d05
hrishabh963 <hrishabhkumar634@gmail.com>
2023-10-14 14:59:24 +0530

    Adjusted css properties

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,8 +5,8 @@ const Header = () => {
     document.documentElement.classList.toggle('dark');
   }
   return (
-    <nav className='flex justify-between w-screen h-28 drop-shadow-lg shadow-inner  px-14 items-center bg-White dark:bg-Dark_Blue'>
-        <h1 className='text-3xl font-extrabold text-Very_Dark_Blue_Light_text dark:text-White'>Where in the world?</h1>
+    <nav className='flex justify-between w-screen h-28 drop-shadow-lg shadow-inner px-3 desktop:px-14 items-center bg-White dark:bg-Dark_Blue'>
+        <h1 className='text-xl desktop:text-3xl font-extrabold text-Very_Dark_Blue_Light_text dark:text-White'>Where in the world?</h1>
          <div className='flex cursor-pointer' onClick={toggleDarkMode}><FontAwesomeIcon icon={faMoon} className='mt-1 dark:hidden' /> 
          <FontAwesomeIcon icon={faMoon} className='mt-1 hidden dark:flex' inverse /> 
          <p className='px-2 text-Very_Dark_Blue_Light_text font-extrabold dark:text-White'>Dark mode</p></div>
--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -6,7 +6,7 @@ const Input = ({handleChange,handleSelect}) => {
       <div className='my-8 desktop:basis-[40%]'>
       <input type="text" name="country" className='rounded-lg drop-shadow-md shadow-inner px-4 py-4 w-11/12 h-20 desktop:h-auto accent-Dark_Gray  text-Dark_Gray caret-Dark_Gray dark:bg-Dark_Blue dark:text-White dark:caret-White dark:placeholder:text-White dark:border-Dark_Blue dark:accent-Dark_Blue desktop:bg-search_light_mode desktop:bg-no-repeat desktop:bg-White desktop:bg-[center_left_1rem] desktop:bg-opacity-100 desktop:pl-[3.3rem] desktop:dark:bg-search_dark_mode cursor-text' placeholder='Search for a country... ' onChange={(event)=> handleChange(event.target.value)} />
       </div>
-        <select name='Filter' defaultValue="Filter By Region"  className='h-16 bg-White drop-shadow-md shadow-inner p-4 w-7/12 rounded-lg desktop:basis-[20%]   desktop:h-14 desktop:mt-8 desktop:mr-10 dark:bg-Dark_Blue dark:text-White cursor-pointer' onChange={(event)=>handleSelect(event.target.value)}>
+        <select name='Filter' defaultValue="Filter By Region"  className='h-16 bg-White drop-shadow-md shadow-inner p-4 w-7/12 rounded-lg desktop:basis-[20%] desktop:h-14 desktop:mt-8 desktop:mr-10 dark:bg-Dark_Blue dark:text-White cursor-pointer' onChange={(event)=>handleSelect(event.target.value)}>
           <option value="" hidden defaultChecked={true}>Filter By Region</option>
           <option value="Africa">Africa</option>
           <option value="America">America</option>
