ac0a5fccecc93c087ebc7f3443fef20eef9a1a74
hrishabh963 <hrishabhkumar634@gmail.com>
2023-10-16 11:04:16 +0530

    loading fix

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loading = () => {
   return (
     <div className='flex justify-center min-h-fit'>
-      <img src="/src/Components/assets/icons8-loading-100.png" alt="loading" className='animate-spin' /> <h1 className='text-4xl font-semibold text-Very_Dark_Blue_Light_text ml-4 py-4 mt-3'>Loading</h1>
+      <img src="./src/Components/assets/icons8-loading-100.png" alt="loading" className='animate-spin' /> <h1 className='text-4xl font-semibold text-Very_Dark_Blue_Light_text ml-4 py-4 mt-3'>Loading</h1>
     </div>
   )
 }
