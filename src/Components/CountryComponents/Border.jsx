9341323f26c235aaf8b0a5a0f73a6e31a8c5ea1e
hrishabh963 <hrishabhkumar634@gmail.com>
2023-10-19 18:07:05 +0530

    Added padding in country borders section

--- src/Components/CountryComponents/Border.jsx
+++ src/Components/CountryComponents/Border.jsx
@@ -35,7 +35,7 @@ const Border = ({borderCountries}) => {
   //JSX render logic
   return (
     <div onClick={handleClick} className='flex flex-col desktop:flex-row '>
-      <p className='flex items-center desktop:justify-center text-Very_Dark_Blue_Light_text dark:text-White font-extrabold '>Border Countries :</p> 
+      <p className='flex pt-4 desktop:justify-center text-Very_Dark_Blue_Light_text dark:text-White font-extrabold '>Border Countries :</p> 
       <div className='flex flex-wrap desktop:pl-2 w-[90vw] desktop:w-[33vw]'>
       {borderCountryNames.length>0  && borderCountryNames.map((country,index)=>{
         return <p key={index} id={country.code} className='border_countries cursor-pointer flex justify-center items-center m-2 h-9 w-24 transform ease-in-out duration-150 text-center hover:-translate-y-2 text-sm font-light text-Very_Dark_Blue_Light_text dark:text-White rounded-md bg-White dark:bg-Dark_Blue drop-shadow-md shadow-inner'>{country.name}</p>
